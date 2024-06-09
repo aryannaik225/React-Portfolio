67cc4ccb23ea2f1fb04c6f7fd4697f6ce0719442
Aryan Naik <aryann2203@gmail.com>
2024-06-09 18:24:50 +0530

    shifted everything here

---
+++ my-portfolio/app/page.tsx
@@ -0,0 +1,28 @@
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Hero from "../components/Hero";
+import { FaHome } from "react-icons/fa";
+import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main className="relative font-extralight bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems}/>
+        <Hero />
+        {/* We basically called the Hero.tsx component in here */}
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+}
---
+++ my-portfolio/components/ui/BentoGrid.tsx
@@ -0,0 +1,172 @@
+'use client'
+
+import { cn } from "@/utils/cn";
+import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GridGlobe"
+import { div } from "three/examples/jsm/nodes/Nodes.js";
+import Lottie from "react-lottie";
+import { useState } from "react";
+import animationData from '@/data/confetti.json';
+import MagicButton from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
+
+export const BentoGrid = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+    // There was a problem here in the video... edited to this and it worked
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto ",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItem = ({
+  className,
+  title,
+  description,
+  id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
+}: {
+  className?: string;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  header?: React.ReactNode;
+  icon?: React.ReactNode;
+  id?: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
+}) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('aryann2203@gmail.com');
+
+    setCopied(true);
+  }
+
+  return (
+    <div
+      className={cn(
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
+        className
+      )}
+      style={{
+        background: 'rgb(4,7,29)',
+        backgroundColor: 'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%',
+      }}
+    >
+        {/* This below part says, if the id = 6 then is should be flex justify center 
+            In js if(something == something) is written as ${something === something} 
+            if the condition is true then what to do is written after the && 
+            the else statement can be written after the && statement using || between them */}
+      <div className={`${id === 6 && 'flex justify-center'} h-full`}>
+          <div className="w-full h-full absolute">
+            {img && (
+              <img
+                src={img}
+                alt={img}
+                className={cn(imgClassName, "object-cover, object-center")}
+              />
+            )}
+          </div>
+
+          <div className={`absolute right-0 -bottom-5 ${id === 5} && 'w-full opacity-80'}`}>
+            {spareImg && (
+              <img
+                src={spareImg}
+                alt={spareImg}
+                className={"object-cover, object-center w-full h-full"}
+              />
+            )}
+          </div>
+
+          {id === 6 && (
+            <BackgroundGradientAnimation>
+              {/* <div className="absolute z-50 flex items-center justify-center text-white font-bold"/> */}
+            </BackgroundGradientAnimation>
+          )}
+
+          <div className={cn(
+            titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 ld:p-10'
+          )}>
+              <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+                {description}
+              </div>
+              <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+                {title}
+              </div>
+          
+
+          {id === 2 && <GlobeDemo />}
+
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-3 w-fit absolute -right-3 lg:-right-2">
+              {/* tech stack lists */}
+              <div className="flex flex-col gap-3 md:gap-auto lg:gap-8">
+                {['React.js', 'Next.js', 'TypeScript'].map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              </div>
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                {['Python', 'Java', 'C'].map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0`}>
+                <Lottie options={{
+                  loop: copied, 
+                  autoplay: copied,
+                  animationData,
+                  rendererSettings: {
+                    preserveAspectRatio: 'xMidYMid slice',
+                  }
+                }} />
+              </div>
+              <MagicButton 
+                title={copied ? 'Email copied' : 'Copy my email'}
+                icon = {<IoCopyOutline/>}
+                position="left"
+                otherClasses="!bg-[#161a31]"
+                handleClick={handleCopy}
+              />
+            </div> 
+          )}
+      </div>
+    </div>
+    </div>
+  );
+};
